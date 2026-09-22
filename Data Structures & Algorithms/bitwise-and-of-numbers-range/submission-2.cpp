f450f264321dd0886535677e2eec682d2aed9963
krith31 <166121605+krith31@users.noreply.github.com>
2026-09-22 13:23:15 +0530

    Add: bitwise-and-of-numbers-range - submission-2

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int result=left;
+        for(int i=left+1;i<=right;i++){
+            result=result&i;
+        }
+        return result;
+    }
+};
